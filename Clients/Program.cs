3aa4af270d089ee68db56340e78d5ad088c4c317
Youssef Amr <yamr0174@gmail.com>
2024-03-25 17:51:17 +0200

    first commit

---
+++ Clients/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint ip = new IPEndPoint(IPAddress.Parse("192.168.1.216"), 2000);
+        try
+        {
+            socket.Connect(ip);
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Connection was Successful ..");
+            Console.ResetColor();
+            Thread th = new Thread(() => ListenAndPrint(socket));
+            th.Start();
+            ReadMessagefromConsole(socket);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+    static void ReadMessagefromConsole(Socket socket)
+    {
+        Console.WriteLine("Please enter your name :");
+        string name = Console.ReadLine();
+        Console.BackgroundColor= ConsoleColor.DarkRed;
+        Console.WriteLine($"Hello {name} ' entet 'exit' to Exit '");
+        Console.ResetColor();
+        while (true)
+        {
+            string msg = Console.ReadLine();
+            if (msg == "exit")
+                break;
+
+            string message = $"{name}:{msg}";
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            socket.Send(buffer);
+        }
+    }
+    static void ListenAndPrint(Socket socket)
+    {
+        byte[] buffer = new byte[1024];
+        try
+        {
+            while (true)
+            {
+                int size = socket.Receive(buffer);
+                if (size > 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, size);
+                    Console.WriteLine($"{message}");
+                }
+                if (size == 0)
+                    break;
+            }
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+class Program
+{
+    static Socket[] numofClient = new Socket[100];
+    static List<string> messegebeforconnect = new List<string>();
+    static int cnt = 0; // count accepted client ;
+    static void Main(string[] args)
+    {
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint ip  = new IPEndPoint(IPAddress.Any, 2000);
+        server.Bind(ip);//cheak the ip and protocol good to use
+        Console.BackgroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("The server has been created successfully...");
+        Console.ResetColor();
+        server.Listen(100);
+        AcceptedConnections(server);
+        server.Shutdown(SocketShutdown.Both);
+        server.Close();
+    }
+    static void AcceptedConnections(Socket server)
+    {
+        while (true)
+        {
+            Socket client = server.Accept();
+            numofClient[cnt++] = client;
+            foreach (string i in messegebeforconnect)
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(i);
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine( client.Send(buffer));
+            }
+            Console.ResetColor();
+            Thread thread = new Thread(() => PrintDate(client));
+            thread.Start();
+        }
+    }
+    static void PrintDate(Socket socket)
+    {
+        byte[] buffer = new byte[1024];
+        while (true)
+        {
+            int size = socket.Receive(buffer);
+            if (size > 0)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, size);// convert byte to string 
+                Console.WriteLine(message);
+                SendMessageToClients(message, socket);
+                messegebeforconnect.Add(message);
+            }
+            if (size == 0)
+                break;
+        }
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+    static void SendMessageToClients(string message, Socket usersender)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+        foreach (Socket i in numofClient)// send message to user connecting only
+        {
+            if (i != null && i != usersender)
+            {
+                i.Send(buffer);
+            }
+        }
+    }
+}
